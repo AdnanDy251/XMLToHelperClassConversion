4d00862449b3bfca210fedfa5a33a4de2d9539ef
AdnanDy251 <144021555+AdnanDy251@users.noreply.github.com>
2023-09-05 15:43:42 +0200

    Add files via upload

---
+++ XMLToHelperClass/Commands/MyCommand.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TextManager.Interop;
+using System.Windows.Forms;
+
+namespace XMLToHelperClass
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var doc =await VS.Documents.GetActiveDocumentViewAsync();
+
+            Clipboard.SetText(doc.TextView.ToString());
+
+        }
+    }
+}
