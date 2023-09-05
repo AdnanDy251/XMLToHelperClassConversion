e49dfbf2aa0d79e234e2fe209d8e9348af193a6a
Adnan Džindo <adnan.dzindo@skyline.be>
2023-09-05 16:37:20 +0200

    test

--- XMLToHelperClass/Commands/MyCommand.cs
+++ XMLToHelperClass/Commands/MyCommand.cs
@@ -4,7 +4,7 @@ using System.Windows;
 namespace XMLToHelperClass
 {
     [Command(PackageIds.MyCommand)]
-    internal sealed class MyCommand : BaseCommand<MyCommand>
+    internal sealed class MyCommand : BaseCommand<MyCommand> 
     {
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
